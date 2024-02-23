0150cff3522b0eb522e8d91631c4eb219e137fdb
Emil Maksudov <emil4ikmolor@gmail.com>
2024-02-23 16:32:50 +0300

    Добавьте файлы проекта.

---
+++ Lerner_Trap/Program.cs
@@ -0,0 +1,68 @@
+﻿using Lerner_Trap;
+
+var dict = new Dictionary<int, List<Trapezoid>>();
+var search = new Stack<Trapezoid>();
+var result = new Stack<Trapezoid>();
+
+var lines = File.ReadAllLines("9.txt");
+int count = int.Parse(lines[0]);
+int i = 1;
+for (; i <= count; i++)
+{
+    var trapezoid = Trapezoid.Parse(i, lines[i]);
+    if (dict.TryGetValue(trapezoid.LeftAngle, out var list))
+    {
+        list.Add(trapezoid);
+    }
+    else
+    {
+        dict.Add(trapezoid.LeftAngle, new List<Trapezoid>() { trapezoid });
+    }
+}
+
+var res = FindPartialCycles(0);
+
+if (!res)
+{
+    File.WriteAllText("OUTPUT.txt", "0");
+    return;
+}
+
+var array = new Trapezoid[count];
+i = 0;
+while (result.Count != 0)
+{
+    array[i++] = result.Pop();
+}
+File.WriteAllText("OUTPUT.txt", string.Join(' ', (object?[])array));
+
+
+bool FindPartialCycles(int start)
+{
+    int temp = start;
+    while (true)
+    {
+        if (!dict!.TryGetValue(temp, out var list))
+        {
+            return false;
+        }
+        var edge = list.FirstOrDefault(x => !x.Visited);
+        if (edge == null)
+        {
+            if (temp != start)
+            {
+                return false;
+            }
+            if (search.Count == 0)
+            {
+                return result.Count == count;
+            }
+            var last = search!.Pop();
+            result!.Push(last);
+            return FindPartialCycles(last.LeftAngle);
+        }
+        search!.Push(edge);
+        temp = edge.RightAngle;
+        edge.Visited = true;
+    }
+}
---
+++ Lerner_Trap/Trapezoid.cs
@@ -0,0 +1,28 @@
+﻿namespace Lerner_Trap;
+
+sealed class Trapezoid
+{
+    public int Index { get; private set; }
+    public int LeftAngle { get; private set; }
+    public int RightAngle { get; private set; }
+    public bool Visited { get; set; }
+    public Trapezoid(int index, int xb, int xc, int xd)
+    {
+        Index = index;
+        LeftAngle = xb;
+        RightAngle = xc - xd;
+        Visited = false;
+    }
+
+    public static Trapezoid Parse(int index, string s)
+    {
+        var split = s.Split(' ');
+        var xb = int.Parse(split[0]);
+        var xc = int.Parse(split[1]);
+        var xd = int.Parse(split[2]);
+        return new Trapezoid(index, xb, xc, xd);
+    }
+
+    public override string ToString()
+        => Index.ToString();
+}
